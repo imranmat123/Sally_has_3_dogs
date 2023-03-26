e16d1bc4fefcbfcd015c9749e86b70adc2bd34ca
Imran <imranmatloob@live.co.uk>
2023-03-26 17:12:14 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    int x {3};
+
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    std::cout << "Sally has " << x << " dogs";
+
+
+    return 0;
+}
+}
